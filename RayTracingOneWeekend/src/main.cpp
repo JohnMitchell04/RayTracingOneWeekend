808161f4b3e093d7c4741a8a935d053aa348c544
AuntieEmphysema <johnrpm1@gmail.com>
2022-05-13 17:14:36 +0100

    Frinish parellising rendering

--- RayTracingOneWeekend/src/main.cpp
+++ RayTracingOneWeekend/src/main.cpp
@@ -8,6 +8,8 @@
 #include "moving_sphere.h"
 
 #include <iostream>
+#include <future>
+#include <thread>
 
 colour ray_colour(const ray& r, const hittable& world, int depth) {
     hit_record rec;
@@ -28,7 +30,8 @@ colour ray_colour(const ray& r, const hittable& world, int depth) {
     return (1.0 - t) * colour(1.0, 1.0, 1.0) + t * colour(0.5, 0.7, 1.0);
 }
 
-hittable_list random_scene() {
+hittable_list random_scene()
+{
     hittable_list world;
 
     auto ground_material = make_shared<lambertian>(colour(0.5, 0.5, 0.5));
@@ -77,8 +80,47 @@ hittable_list random_scene() {
     return world;
 }
 
-int main() {
+// Mutex to prevent pixels being written and console log output at the same time
+static std::mutex output_mutex;
 
+// Calulate a row of pixels
+static void calculate_pixels(
+    std::vector<std::vector<std::tuple<int, int, colour>>>& rows, int image_width, int image_height,
+    int samples_per_pixel, hittable_list world, camera cam, int max_depth, int j
+)
+{
+    std::vector<std::tuple<int, int, colour>> current_row;
+    for (int i = 0; i < image_width; ++i)
+    {
+        colour pixel_colour(0, 0, 0);
+        for (int s = 0; s < samples_per_pixel; ++s)
+        {
+            auto u = ((i + random_double()) / (image_width - 1));
+            auto v = ((j + random_double()) / (image_height - 1));
+            ray r = cam.get_ray(u, v);
+            pixel_colour += ray_colour(r, world, max_depth);
+        }
+        current_row.push_back(std::make_tuple(j, i, pixel_colour));
+    }
+    std::lock_guard<std::mutex> lock(output_mutex);
+    rows.push_back(current_row);
+    std::cerr << "Scanline: " << j << std::endl;
+}
+
+// Wakes the conditional variable when we have finished calculating all rows
+static void wake_main_thread(std::vector<std::vector<std::tuple<int, int, colour>>>& rows, std::condition_variable& cv, int image_height)
+{
+    using namespace std::chrono_literals;
+    while (rows.size() != image_height)
+    {
+        std::this_thread::sleep_for(1s);
+    }
+
+    cv.notify_all();
+}
+
+int main()
+{
     // Image
     const auto aspect_ratio = 16.0 / 9.0;
     const int image_width = 400;
@@ -100,19 +142,35 @@ int main() {
 
     // Render
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+    std::vector<std::vector<std::tuple<int, int, colour>>> rows;
 
-    for (int j = image_height - 1; j >= 0; --j) {
-        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
-        for (int i = 0; i < image_width; ++i) {
-            colour pixel_colour(0, 0, 0);
-            for (int s = 0; s < samples_per_pixel; ++s)
-            {
-                auto u = ((i + random_double()) / (image_width - 1));
-                auto v = ((j + random_double()) / (image_height - 1));
-                ray r = cam.get_ray(u, v);
-                pixel_colour += ray_colour(r, world, max_depth);
-            }
-            write_colour(std::cout, pixel_colour, samples_per_pixel);
+    std::vector<std::future<void>> future;
+    std::mutex threads;
+    std::condition_variable cv;
+
+    // Async call all rows to be calculated
+    for (int j = image_height - 1; j >= 0; --j)
+    {
+        future.push_back(std::async(std::launch::async, std::ref(calculate_pixels),
+            std::ref(rows), image_width, image_height, samples_per_pixel, world, cam, max_depth, j)
+        );
+    }
+    // Add the wake main thread and wait until it does
+    future.push_back(std::async(std::launch::async, std::ref(wake_main_thread), std::ref(rows), std::ref(cv), image_height));
+    std::unique_lock<std::mutex> lck(threads);
+    cv.wait(lck);
+
+    // Sort the calculated rows based on j coord
+    std::sort(rows.begin(), rows.end(), [](const std::vector<std::tuple<int, int, colour>>& a, const std::vector<std::tuple<int, int, colour>>& b) {
+        return std::get<0>(a.front()) < std::get<0>(b.front());
+    });
+
+    // Write the pixels to the output file
+    for (int j = image_height - 1; j >= 0; --j)
+    {
+        for (int i = 0; i < image_width; i++)
+        {
+            write_colour(std::cout, std::get<2>(rows.at(j).at(i)), samples_per_pixel);
         }
     }
 
